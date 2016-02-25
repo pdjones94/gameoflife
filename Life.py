720e119681f57d0575dc66ea25a309f6dfbac5a1
pdjones94 <pdjones94@gmail.com>
2016-02-25 23:54:44 +0000

    Delete Life.py

--- Life.py
+++
@@ -1,8 +0,0 @@
-import sys
-class Life(object):
-  
-  def __init__(self, initialState):
-      self.live_cells = initialState
-      
-  def cell_should_survive(self, num_neighbours):
-      sys.exit("Not Implemented")
